fab0bd983109c6bc68d497dacde3a1bdcf1c9bcf
AMABHISHEKA <76518816+AMABHISHEKA@users.noreply.github.com>
2021-07-07 12:20:13 +0530

    Add files via upload

---
+++ Javascript_essentials/Assignment-1/assignment-1.js
@@ -0,0 +1,10 @@
+/* Day-2 Javascript Essentail Program
+ Program code By: ABHISHEKA A M  */
+ 
+ // Program to print 5 times "*" Horizontally in single line
+
+ let i,str="";
+ for(i=0;i<5;i++){
+     str+="*";
+ }
+ console.log(str);
