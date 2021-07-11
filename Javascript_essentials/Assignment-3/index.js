a85d69f17b9a32779de3f5e1817461a386492147
AMABHISHEKA <76518816+AMABHISHEKA@users.noreply.github.com>
2021-07-11 12:43:13 +0530

    Add files via upload

---
+++ Javascript_essentials/Assignment-3/index.js
@@ -0,0 +1,9 @@
+// Day-4 Javascript Essential Program
+// Program code by Abhisheka A M  
+
+function on_click() {
+    let a = document.getElementById('one');
+    let b = a.value;
+
+    document.getElementById('input').innerText="Hello" +b;
+}
